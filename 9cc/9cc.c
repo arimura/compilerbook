6b4bbbea698ed903f4b7a42195a46d691125a736
arimura <33759663+arimura@users.noreply.github.com>
2024-04-20 22:46:45 +0900

    Tokenize

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -17,15 +17,15 @@ struct Token {
     TokenKind kind;
     Token *next;
     int val;
-    chat *str;
-}
+    char *str;
+};
 
 Token *token;
 
 void error(char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
-    vfpring(stderr, fmt, ap);
+    vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
 }
@@ -63,6 +63,34 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
     return tok;
 }
 
+Token *tokenize(char *p) {
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while(*p) {
+        if(isspace(*p)){
+            p++;
+            continue;
+        }
+
+        if( *p == '+' || *p == '-') {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+
+        if(isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error("トークナイズできません");
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
 
 
 
@@ -72,28 +100,21 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    char *p = argv[1];
-
+    token = tokenize(argv[1]);
+    
     printf(".intel_syntax noprefix\n");
-    printf(".global main\n");
-    printf("main: \n");
-    printf("    mov rax, %ld\n", strtol(p, &p, 10));
+    printf(".globl main\n");
+    printf("main:\n");
+    printf("    mov rax, %d\n", expect_number());
 
-    while (*p) {
-        if (*p == '+') {
-            p++;
-            printf("    add rax, %ld\n", strtol(p, &p, 10));
+    while (!at_eof()) {
+        if(consume('+')) {
+            printf("    add rax, %d\n", expect_number());
             continue;
         }
 
-        if (*p == '-') {
-            p++;
-            printf("    sub rax, %ld\n", strtol(p, &p, 10));
-            continue;
-        }
-
-        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-        return 1;
+        expect('-');
+        printf("    sub rax, %d\n", expect_number());
     } 
 
     printf("    ret\n");
