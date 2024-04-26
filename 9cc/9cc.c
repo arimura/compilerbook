104378a818fd0978998e7dc413b4c0755f4433b3
arimura <33759663+arimura@users.noreply.github.com>
2024-04-26 13:35:47 +0900

    Add gen

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -20,6 +20,7 @@ struct Token {
     char *str;
 };
 
+char *user_input;
 Token *token;
 
 void error(char *fmt, ...) {
@@ -166,30 +167,53 @@ Node *expr(){
     }
 }
 
+void gen(Node *node) {
+    if (node->kind = ND_NUM) {
+        printf("    push %d\n", node->val);
+        return;
+    }
+    
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf(" pop rdi\n");
+    printf(" pop rax\n");
+
+    switch (node->kind){
+    case ND_ADD:
+        printf("    add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("    sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("    imul rax, rdi\n");
+    case ND_DIV:
+        printf(" cqp\n");
+        printf("    idiv rdi\n");
+        break;
+    }
+
+    printf(" push rax\n");
+}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "引数の個数が正しくありません");
+        error("引数の個数が正しくありません");
         return 1;
     }
 
-    token = tokenize(argv[1]);
-    
+    user_input = argv[1];
+    token = tokenize(user_input);
+    Node *node = expr();
+
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("    mov rax, %d\n", expect_number());
 
-    while (!at_eof()) {
-        if(consume('+')) {
-            printf("    add rax, %d\n", expect_number());
-            continue;
-        }
+    gen(node);
 
-        expect('-');
-        printf("    sub rax, %d\n", expect_number());
-    } 
-
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
