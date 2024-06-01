ee33f741827d6539c783db65e284aece86ce594a
arimura <33759663+arimura@users.noreply.github.com>
2024-06-02 08:44:09 +0900

    Handle return

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -12,6 +12,7 @@ typedef enum
     TK_RESERVED,
     TK_INDENT,
     TK_NUM,
+    TK_RETURN,
     TK_EOF,
 } TokenKind;
 
@@ -52,6 +53,7 @@ typedef enum
     ND_NE,
     ND_ASSIGN,
     ND_LVAR,
+    ND_RETURN,
 } NodeKind;
 
 typedef struct Node Node;
@@ -96,7 +98,8 @@ const char *getTokenKindName(TokenKind kind)
         return "TK_EOF";
     case TK_INDENT:
         return "TK_INDENT";
-    // Add more cases as necessary
+    case TK_RETURN:
+        return "TK_RETUREN";
     default:
         return "Unknown";
     }
@@ -145,6 +148,8 @@ const char *getNodeKindName(NodeKind kind)
         return "Assign";
     case ND_LVAR:
         return "LocalVariable";
+    case ND_RETURN:
+        return "Return";
     default:
         return "Unknown";
     }
@@ -217,6 +222,14 @@ Token *consume_ident(){
     return t;
 }
 
+bool consume_kind(TokenKind kind){
+    if(token->kind != kind){
+        return false;
+    }
+    token = token->next;
+    return true;
+}
+
 void expect(char op)
 {
     if (token->kind != TK_RESERVED || token->str[0] != op)
@@ -251,6 +264,13 @@ bool startswith(char *p, char *q) {
   return memcmp(p, q, strlen(q)) == 0;
 }
 
+int is_alnum(char c) {
+    return ('a' <= c && c <= 'z') ||
+           ('A' <= c && c <= 'Z') ||
+           ('0' <= c && c <= '9') ||
+           (c == '_');
+}
+
 Token *tokenize(char *p)
 {
     Token head;
@@ -265,8 +285,15 @@ Token *tokenize(char *p)
             continue;
         }
 
+        if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])){
+           cur = new_token(TK_RETURN, cur, p);
+           cur->len = 6;
+           p = p + 6;
+           continue; 
+        }
+
         if ('a' <= *p && *p <= 'z'){
-            cur = new_token(TK_INDENT, cur, p++);
+        cur = new_token(TK_INDENT, cur, p++);
             cur->len = 1;
             continue;
         }
@@ -335,7 +362,16 @@ Node *expr()
 }
 
 Node *stmt(){
-    Node *node = expr();
+    Node *node;
+    
+    if(consume_kind(TK_RETURN)){
+        node = calloc(1, sizeof(Node));
+        node->kind = ND_RETURN;
+        node->lhs = expr();
+    }else{
+        node = expr();
+    } 
+    
     expect(';');
     return node;
 }
@@ -495,6 +531,12 @@ void gen(Node *node)
         printf("    mov [rax], rdi\n");
         printf("    push rdi\n");
         return;
+    case ND_RETURN:
+        gen(node->lhs);
+        printf("    pop rax\n");
+        printf("    mov rsp, rbp\n");
+        printf("    pop rbp\n");
+        printf("    rent\n");
     }
 
     gen(node->lhs);
@@ -553,9 +595,9 @@ int main(int argc, char **argv)
 
     user_input = argv[1];
     token = tokenize(user_input);
-    printToken(token);
+    // printToken(token);
     program();
-    printLocals();
+    // printLocals();
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
