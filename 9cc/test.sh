68d9faa8d71c11f433e3e4b5b308f7f8dbcca8dd
arimura <33759663+arimura@users.noreply.github.com>
2024-07-02 20:38:05 +0900

    Mod test

--- 9cc/test.sh
+++ 9cc/test.sh
@@ -65,6 +65,6 @@ assert 2 "hoge(x){return x;} main(){return hoge(2);}"
 assert 5 "hoge(x, y){return x + y;} main(){return hoge(2, 3);}"
 assert 1 "main(){qc_print(10);return 1;}"
 assert 1 "fib(i,s){ if(i == 0){return 1;}  return fib(i-1,0);} main(){return fib(1,0);}"
-assert 1 "fib(p, n, i){if(i == 0){return 1;}  return fib(n, p + n, i -1) ;} main(){return fib(1,1,3);}"
+assert 5 "fib(p, n, i){if(i == 0){return n;}  return fib(n, p + n, i-1) ;} main(){return fib(1,1,3);}"
 # assert 1 "fib(p, n, i){if(i == 0){return 1;} qc_print(10); return fib(n, p + n, i -1) ;} main(){return fib(1,1,3);}"
 echo OK
