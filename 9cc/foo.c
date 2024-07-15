55bddd01f2557db6f179894532e5a42a0ae6011b
arimura <33759663+arimura@users.noreply.github.com>
2024-07-15 20:15:01 +0900

    Add malloc api

--- 9cc/foo.c
+++ 9cc/foo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int foo(int x, int y)
 {
@@ -8,4 +9,23 @@ int foo(int x, int y)
 int qc_print(int x)
 {
     printf("pc_print: %d\n", x);
+}
+
+void alloc4(void **p, int h, int i, int j, int k)
+{
+    void *a = malloc(4 * sizeof(int));
+
+    if (a == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed!\n");
+        exit(1);
+    }
+
+    int *b = a;
+    b[0] = h;
+    b[1] = i;
+    b[2] = j;
+    b[3] = k;
+
+    *p = a;
 }
