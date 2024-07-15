2f0ac1e7122cfa0ae534da2884cca099d94c7940
arimura <33759663+arimura@users.noreply.github.com>
2024-07-15 13:04:20 +0900

    Fix

--- 9cc/codegen.c
+++ 9cc/codegen.c
@@ -3,9 +3,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-//Generates the assembly code to push the address of a local variable onto the stack.
-void gen_lval(Node *node){
-    if(node->kind != ND_LVAR)
+// Generates the assembly code to push the address of a local variable onto the stack.
+void gen_lval(Node *node)
+{
+    if (node->kind != ND_LVAR)
         error("代入の左辺値が変数ではありません");
 
     printf("# gen_lval\n");
@@ -15,14 +16,16 @@ void gen_lval(Node *node){
     printf("# gen_lval end\n");
 }
 
-static int count(void) {
+static int count(void)
+{
     static int i = 1;
     return i++;
 }
 void gen(Node *node)
 {
     // fprintf(stderr, "gen: %d\n", node->kind);
-    switch(node->kind){
+    switch (node->kind)
+    {
     case ND_NUM:
         printf("    push %d\n", node->val);
         return;
@@ -35,15 +38,23 @@ void gen(Node *node)
         printf("# lvar value end\n");
         return;
     case ND_ASSIGN:
-        //genの再帰で扱う？
-        // Node *var = node->lhs;
-        // while(var->kind == ND_DEREF){
-        //     var = var->lhs;
-        // }
-        if(node->lhs->kind == ND_DEREF)
+        //To be refactored.
+        //複数のderef(**y = 3;)に対応しているか未確認
+        if (node->lhs->kind == ND_DEREF)
         {
-            gen(node->lhs);
-        }else{
+            Node *var = node->lhs;
+            while (var->kind == ND_DEREF)
+            {
+                var = var->lhs;
+            }
+            printf("# gen_deref_lval\n");
+            printf("    mov rax, rbp\n");
+            printf("    sub rax, %d\n", var->offset);
+            printf("    push [rax]\n");
+            printf("# gen_dereflval end\n");
+        }
+        else
+        {
             gen_lval(node->lhs);
         }
         gen(node->rhs);
@@ -70,11 +81,12 @@ void gen(Node *node)
         printf("    je  .Lelse%d\n", c);
         gen(node->then);
         printf("    jmp  .Lend%d\n", c);
-        printf(".Lelse%d:\n",c);
-        if(node->els){
+        printf(".Lelse%d:\n", c);
+        if (node->els)
+        {
             gen(node->els);
         }
-        printf(".Lend%d:\n",c);
+        printf(".Lend%d:\n", c);
         return;
     case ND_WHILE:
         int cw = count();
@@ -89,20 +101,25 @@ void gen(Node *node)
         return;
     case ND_FOR:
         int cf = count();
-        if(node->init){
+        if (node->init)
+        {
             gen(node->init);
         }
         printf(".Lbegin%d:\n", cf);
-        if(node->cond){
+        if (node->cond)
+        {
             gen(node->cond);
-        }else{
+        }
+        else
+        {
             printf("    push 1\n");
         }
         printf("    pop rax\n");
         printf("    cmp rax, 0\n");
         printf("    je .Lend%d\n", cf);
         gen(node->then);
-        if(node->inc){
+        if (node->inc)
+        {
             gen(node->inc);
         }
         printf("    jmp .Lbegin%d\n", cf);
@@ -110,7 +127,8 @@ void gen(Node *node)
         return;
     case ND_BLOCK:
         Node *n = node->body;
-        while(n){
+        while (n)
+        {
             gen(n);
             printf("    pop rax\n");
             n = n->next;
@@ -118,19 +136,21 @@ void gen(Node *node)
         return;
     case ND_FUNCALL:
         Node *a = node->args;
-        //push args
+        // push args
         int ac = 0;
-        while(a && ac < 6){
+        while (a && ac < 6)
+        {
             gen(a);
             a = a->next;
             ac++;
         }
 
         int i = 0;
-        //System V ABIでは6つのregister以上の引数を利用する場合はrspを16の倍数にする必要がある。
-        //今はregisterのみ利用
+        // System V ABIでは6つのregister以上の引数を利用する場合はrspを16の倍数にする必要がある。
+        // 今はregisterのみ利用
         const char *r[] = {"rdi", "rsi", "rdx", "rcx", "r8", "r9"};
-        while(i < ac){
+        while (i < ac)
+        {
             printf("    pop %s\n", r[ac - i - 1]);
             i++;
         }
@@ -145,7 +165,7 @@ void gen(Node *node)
         char *f = malloc((node->funcname_len + 1) * sizeof(char));
         strncpy(f, node->funcname, node->funcname_len);
         f[node->funcname_len] = '\0';
-        
+
         printf("%s:\n", f);
         printf("# prologue\n");
         printf("    push rbp\n");
@@ -156,7 +176,8 @@ void gen(Node *node)
         Node *fa = node->args;
         const char *fr[] = {"rdi", "rsi", "rdx", "rcx", "r8", "r9"};
         int fi = 0;
-        while(fa && fi < 6){
+        while (fa && fi < 6)
+        {
             gen_lval(fa);
             printf("    pop rax\n");
             printf("    mov [rax], %s\n", fr[fi]);
@@ -169,7 +190,7 @@ void gen(Node *node)
         printf("# epilogue\n");
         printf("    mov rsp, rbp\n");
         printf("    pop rbp\n");
-        printf("    ret\n");        
+        printf("    ret\n");
         printf("# epilogue end\n");
 
         return;
@@ -181,7 +202,7 @@ void gen(Node *node)
         printf("# deref\n");
         printf("    pop rax\n");
         printf("    mov rax, [rax]\n");
-        printf("    push rax\n"); 
+        printf("    push rax\n");
         printf("# deref\n");
         return;
     }
