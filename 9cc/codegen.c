73c7bcbfe6eb5fcb6e78b49413377fcea04b6570
arimura <33759663+arimura@users.noreply.github.com>
2024-07-15 11:31:37 +0900

    Add commnet

--- 9cc/codegen.c
+++ 9cc/codegen.c
@@ -28,9 +28,11 @@ void gen(Node *node)
         return;
     case ND_LVAR:
         gen_lval(node);
+        printf("# lvar value\n");
         printf("    pop rax\n");
         printf("    mov rax, [rax]\n");
         printf("    push rax\n");
+        printf("# lvar value end\n");
         return;
     case ND_ASSIGN:
         //genの再帰で扱う？
