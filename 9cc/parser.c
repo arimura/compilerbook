a225c52d0ee23504421813caf1a943507ba1bdc7
arimura <33759663+arimura@users.noreply.github.com>
2024-07-30 23:08:56 +0900

    refactor

--- 9cc/parser.c
+++ 9cc/parser.c
@@ -140,10 +140,7 @@ Node *declare_lvar()
     }
 
     // 変数名の左側の型情報（"int *i[3]"の"int *"の部分）
-    //  現時点ではintのみ
     Type *base = calloc(1, sizeof(Type));
-    //TODO: support char on gvar
-    fprintf(stderr, "token str: %s\n", token->str);
     if(strncmp(type->str, "int", 3) == 0)
     {
         base->ty = INT;
@@ -266,15 +263,22 @@ Node *declare_func()
 
 Node *declare_gvar()
 {
-    if (!consume_kind(TK_TYPE))
+    Token *type = consume_type();
+    if (!type)
     {
         return NULL;
     }
 
     // 変数名の左側の型情報（"int *i[3]"の"int *"の部分）
-    //  現時点ではintのみ
     Type *base = calloc(1, sizeof(Type));
+    if(strncmp(type->str, "int", 3) == 0)
+    {
     base->ty = INT;
+    }
+    else if(strncmp(type->str, "char", 4) == 0 )
+    {
+    base->ty = CHAR;
+    }
     Type *head = calloc(1, sizeof(Type));
     Type *c = head;
     while (consume("*"))
